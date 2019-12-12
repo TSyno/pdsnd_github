fb2f67d9fdae206e548300c7fe0376629e62900e
Oluwatosin Oyelese <tosinoyelese@gmail.com>
2019-12-12 15:38:59 +0100

    doc: Specified available options for month

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,7 @@ def get_filters():
 
     Returns:
         (str) city - name of the city to analyze (Chicago, Washington or New York)
-        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) month - name of the month to filter by (January to June), or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
